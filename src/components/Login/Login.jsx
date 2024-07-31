6c4f39567b3bc5eb8340cb5a5eb5b1b74fe6686a
kamrulkoche <kamrulkoche@gmail.com>
2024-08-01 01:15:39 +0600

    GoogleAuthProvider add

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,18 @@
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import app from "../../firebase/firebase.init";
+
 const Login = () => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn=()=>{
+    console.log('Google mama is coming');
+  }
   return (
     <div>
-      <button>Google login</button>
+      <button onClick={handleGoogleSignIn}>Google login</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
